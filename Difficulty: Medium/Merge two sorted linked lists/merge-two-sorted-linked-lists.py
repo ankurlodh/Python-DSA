57e6ef4efbc62b1979148e8fe5816b67d09e90e9
Ankur Lodh <ankurlodh25@gmail.com>
2026-05-13 15:38:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
@@ -0,0 +1,25 @@
+'''
+class Node:
+    def __init__(self, data): 
+        self.data = data
+        self.next = None
+
+'''
+class Solution:
+    def sortedMerge(self, head1, head2):
+        newlist=Node(-1)
+        tail=newlist
+        while head1 and head2:
+            if(head1.data<=head2.data):
+                tail.next=head1
+                head1=head1.next
+            else:
+                tail.next=head2
+                head2=head2.next
+            tail=tail.next
+        if head1:
+            tail.next=head1
+        else:
+            tail.next=head2
+        return newlist.next
+        
