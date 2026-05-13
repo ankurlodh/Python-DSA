fc96edb7b684a2336e560b858a370f49a5553c84
Ankur Lodh <ankurlodh25@gmail.com>
2026-05-13 10:40:44 +0530

    Time: 3 ms (85.85%), Space: 13.5 MB (73.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,6 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        d={}
-        for i in nums:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-            if d[i]>len(nums)//2:
-                return i
+class Solution(object):
+    def majorityElement(self, nums):
+        nums.sort()
+        return nums[len(nums)//2]
 
-
+        
